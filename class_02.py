4aa0219ef7925fa90c1cb0037d6fcea614fa2963
certshri <srikant.pawar@certviewtech.com>
2018-11-03 10:32:56 +0530

    intial program

--- class_02.py
+++ class_02.py
@@ -3,5 +3,7 @@ Following things we will cover today
 1) Variable
 2) String,Integer, Boolean data type
 3None Type
-
 """
+#################################
+# 1) Variable
+#################################
