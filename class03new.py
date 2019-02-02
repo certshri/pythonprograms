422d0660ed79ec37477b32f4195d9d1ed8d96af8
certshri <srikant.pawar@certviewtech.com>
2019-02-02 13:15:04 +0530

    create local branch and pushing up

--- class03new.py
+++ class03new.py
@@ -1,1 +1,32 @@
-print('create local branch and now pushing up')
+"""
+[]=List
+{}=Dictionary
+()= Tuple
+"""
+# dict_variable={
+#      'name':'Chandrakant',
+#      'Address':'Sainik Nager',
+#      'Mobile no':9637646900 }
+# print(dict_variable)
+# # print(dict_variable['name'])
+# # print(dict_variable['Mobile no'])
+# print(dict_variable.keys())
+# print(dict_variable.values())
+# print(dict_variable.items())
+# print(dict_variable.fromkeys('name'))
+# print(dict_variable)
+# contact_no=dict_variable['Mobile no']
+# dict_variable.pop('Mobile no')
+# dict_variable['contact_no'] = contact_no
+# print(dict_variable)
+
+
+dict_01 =[
+    {'test': 'test2','test3': 'test4'},
+    {'test5': [
+        {'test6':'test7','test8':'test9'},
+        {'test10':'test11','test12':'test13'}
+              ]
+
+    }
+        ]
